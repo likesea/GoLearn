5e5d2a2c4d84f93058ff351025c560892fac5b73
likesea <kukuyanbin@163.com>
2015-09-22 22:37:29 +0800

    add go

--- src/filesever/main.go
+++ src/filesever/main.go
@@ -1,5 +1,12 @@
 package main
-import "net/http"
-func main(){
-	http.ListenAndServe(":8090", http.FileServer(http.Dir(".")))
+
+import "fmt"
+
+func main() {
+	//http.ListenAndServe(":8090", http.FileServer(http.Dir(".")))
+	var (
+		i int = 2
+		j int = 3
+	)
+	fmt.Println(i, j)
 }
